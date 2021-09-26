a3fcb3aea6ae3bf525d0f5155464464d4b813196
Kit Martin <kit@kitified.dev>
2021-09-26 10:13:50 -0700

    pcap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use pcap::Device;
+
 fn main() {
-    println!("Hello, world!");
+    // get the default Device
+    let device = pcap::Device::lookup().unwrap();
+    println!("Using device {}", device.name);
+
+    // Setup Capture
+    let mut cap = pcap::Capture::from_device(device)
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
+
+    // get a packet and print its bytes
+    println!("{:?}", cap.next());
 }
